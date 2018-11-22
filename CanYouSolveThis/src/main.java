edc2d92e83f18d8d77013cea02ac8e21b3f1f4d9
Oğulcan Tekinalp <ogulcan.tekinalp@ozu.edu.tr>
2018-11-22 22:13:37 +0300

    inicial project commit

---
+++ CanYouSolveThis/src/main.java
@@ -0,0 +1,31 @@
+
+import javax.swing.*;
+import View.*;
+
+import java.awt.*;
+
+public class main {
+
+    public static void main(String[]args){
+            JFrame frame=new JFrame("Can You Solve This");
+            StartScreen startScreen=new StartScreen();
+           JPanel startPanel = startScreen.getPanel();
+                    frame.add(startScreen.getPanel());
+            startScreen.getPanel().setVisible(true);
+
+
+        frame.add(startPanel);
+
+        frame.setSize(1200, 600);
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+
+
+
+
+    }
+}
