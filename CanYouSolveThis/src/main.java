7bf30a8530540cc6960ef1758775d9f76a0f8dc5
Anasimandro <cemtaha2@gmail.com>
2018-11-23 00:54:55 +0300

    Start and main changes

--- CanYouSolveThis/src/main.java
+++ CanYouSolveThis/src/main.java
@@ -7,25 +7,17 @@ import java.awt.*;
 public class main {
 
     public static void main(String[]args){
-            JFrame frame=new JFrame("Can You Solve This");
-            StartScreen startScreen=new StartScreen();
-           JPanel startPanel = startScreen.getPanel();
-                    frame.add(startScreen.getPanel());
-            startScreen.getPanel().setVisible(true);
+        JFrame frame = new JFrame("Can You Solve This");
+        frame.setSize(1000, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        StartScreen startScreen=new StartScreen();
+        JPanel startPanel = startScreen.getPanel();
 
         frame.add(startPanel);
 
-        frame.setSize(1200, 600);
-
-        frame.setLocationRelativeTo(null);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        startPanel.setVisible(true);
         frame.setVisible(true);
-
-
-
-
     }
 }
