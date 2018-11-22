0cb87e4cdb4e7b54be770939bd16250b4c4528e0
eyluldayi <eylul.dayi@ozu.edu.tr>
2018-11-23 01:12:07 +0300

    Play

--- CanYouSolveThis/src/Main.java
+++ CanYouSolveThis/src/Main.java
@@ -23,6 +23,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
+
     }
 
     private void startNewGame() {
--- CanYouSolveThis/src/Model/Game.java
+++ CanYouSolveThis/src/Model/Game.java
@@ -1,6 +1,10 @@
 package Model;
+import java.util.*;
 
 public class Game {
+
+    private int currentQuestionIndex;
+    private Timer timer; //instantiate
 
     public Game() {
 
@@ -24,6 +28,36 @@ public class Game {
     private void showQuestion() {
         //TODO
         //Shows the current question
+        /*
+        * for(currentQuestion;currentQuestion < ...;currentQuestion++)
+        * print question i
+        *
+        * */
+    }
+
+    private void endGame() {
+        //TODO
+        //ask rebound if question 6...
+        //showFinalScore();
+    }
+
+    private void showFinalScore() {
+        //TODO
+        //calculateScore();
+    }
+
+    private int calculateScore() {
+        //TODO
+        //calculation algorithm
+        return 0;
+    }
+
+    private void useJoker() {
+
+    }
+
+    private void rebound() {
+
     }
 
     private void viewPreviousScores() {
