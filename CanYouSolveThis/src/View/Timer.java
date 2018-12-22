20d85d4c33fa338287f0e891fef7a2b463ee95a5
Anasimandro <cemtaha2@gmail.com>
2018-12-22 22:22:35 +0300

    Timer update

--- CanYouSolveThis/src/View/Timer.java
+++ CanYouSolveThis/src/View/Timer.java
@@ -1,4 +1,31 @@
 package View;
 
-public class Timer {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Timer extends javax.swing.Timer{
+    private JPanel panel;
+    private JLabel timeLabel;
+    int N = 60;
+    public Timer(int time, ClockListener cl){
+        super(time, cl);
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        timeLabel = new JLabel();
+
+        panel.add(timeLabel);
+
+    }
+    private class ClockListener implements ActionListener {
+
+        private int count;
+
+        public void actionPerformed(ActionEvent e) {
+            count %= N;
+            timeLabel.setText(String.valueOf(count));
+            count++;
+        }
+    }
 }
