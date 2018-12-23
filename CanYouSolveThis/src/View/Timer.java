9d146652a1fd84c5efaeb2df8b3908a515a68992
Anasimandro <cemtaha2@gmail.com>
2018-12-23 15:45:54 +0300

    Timer update with indepent action listener and Timer constructor with no argument

--- CanYouSolveThis/src/View/Timer.java
+++ CanYouSolveThis/src/View/Timer.java
@@ -9,37 +9,44 @@ public class Timer extends javax.swing.Timer{
     private JPanel panel;
     private JLabel timeLabel;
     private int initialTime; //Time to answer questions.
-    private int counter; //timeListener
 
-    public Timer(int delay, TimeListener timeListener){
-        super(delay, timeListener);
+    public Timer(){
+        super(1000, null); //Timer super constructor takes a delay and actionlistener
+        super.addActionListener(new TimeListener());
         this.initialTime = 600;
-        super.start();
+
         panel = new JPanel();
         panel.setLayout(new BorderLayout());
-
         timeLabel = new JLabel();
-
         panel.add(timeLabel, BorderLayout.CENTER);
+
+        super.start();
     }
 
     public JPanel getPanel() {
         return panel;
     }
+    public JLabel getTimeLabel(){
+        return timeLabel;
+    }
+    public int getInitialTime(){
+        return initialTime;
+    }
 
     private class TimeListener implements ActionListener {
         int secondsRemaining;
+        int counter;
 
         public void actionPerformed(ActionEvent e) {
-            secondsRemaining = initialTime-counter;
-            timeLabel.setText("Time remaining : " + secondsRemaining + "seconds");
+            secondsRemaining = getInitialTime()- counter;
+            getTimeLabel().setText("Time remaining : " + secondsRemaining + "seconds");
 
             if (OutOfTime()) {
                 Timer.super.stop();
             }
         }
         private boolean OutOfTime(){
-            return (++counter > initialTime);
+            return (++counter > getInitialTime());
         }
     }
 }
